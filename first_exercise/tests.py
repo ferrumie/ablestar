aae4d67c6ab616dd3092db7df1f7a498ef91e3a3
John Doe <alabiemmanuelferanmi@gmail.com>
2022-02-26 23:02:24 +0100

    complete logic

--- first_exercise/tests.py
+++ first_exercise/tests.py
@@ -1,21 +1,16 @@
 from unittest import TestCase
 
-from first_exercise.utils.tag_edit import remove_tags
+from first_exercise.utils.tag_edit import add_tags, edit_tags, remove_tags
 
 
 class TestTagEdit(TestCase):
     def setUp(self) -> None:
-        self.current_tags = [
-            "ball",
-            "power",
-            "super",
-            "job",
-            "content"
-        ]
+        self.current_tags = ["ball", "power", "super", "job", "content", "cold show"]
+        self.passed_tags = "ball, power, super, job, Content, cold-show"
 
     def test_remove_tags_function_with_special_characters(self):
-        
-        tags_to_remove = ["b-all", "p_ower", "j ob"]
+
+        tags_to_remove = ["b-all", "power", "j ob", "cold-show"]
         tags = remove_tags(self.current_tags, tags_to_remove)
 
         # b-all should be same as ball and should be removed
@@ -23,6 +18,7 @@ class TestTagEdit(TestCase):
         self.assertNotIn("ball", tags)
         self.assertNotIn("power", tags)
         self.assertNotIn("job", tags)
+        self.assertNotIn("cold show", tags)
 
     def test_remove_tags_function_with_uppercases(self):
         tags_to_remove = ["BALL", "poWeR"]
@@ -32,17 +28,67 @@ class TestTagEdit(TestCase):
         self.assertNotIn("power", tags)
 
     def test_add_tags_that_already_exists(self):
-        #test the perservation of tags
-        pass
+        # test the perservation of tags
 
-    def test_add_tags_with_special_characters(self):
-        pass
+        tags_to_add = ["job"]
+        tags = add_tags(self.current_tags, tags_to_add)
+
+        self.assertEqual(len(self.current_tags), len(tags))
+        # test that job is capitalized
+        self.assertIn("JOB", self.current_tags)
+
+        # test with different variations
+        tags_to_add = ["Job"]
+        tags = add_tags(self.current_tags, tags_to_add)
+
+        self.assertEqual(len(self.current_tags), len(tags))
+
+        # test with different variations
+        tags_to_add = ["JO-b"]
+        tags = add_tags(self.current_tags, tags_to_add)
+
+        self.assertEqual(len(self.current_tags), len(tags))
+
+        # test with different variations
+        tags_to_add = ["cold-show"]
+        tags = add_tags(self.current_tags, tags_to_add)
+
+        self.assertEqual(len(self.current_tags), len(tags))
+
+        # test with different variations
+        tags_to_add = ["cold_show"]
+        tags = add_tags(self.current_tags, tags_to_add)
+
+        self.assertEqual(len(self.current_tags), len(tags))
 
     def test_add_new_tags(self):
-        pass
+        tags_to_add = ["code"]
+        add_tags(self.current_tags, tags_to_add)
+
+        self.assertIn("code", self.current_tags)
+
+        # add tags
 
     def test_edit_tags_with_various_cases(self):
-        pass
+        # remove an added tag
+        tags_to_add = ["title-snow"]
+        tags_to_remove = ["title snow"]
 
-    
+        tags = edit_tags(current_tags=self.passed_tags, tags_to_add=tags_to_add, tags_to_remove=tags_to_remove)
+        self.assertNotIn("title-snow", tags)
 
+        # case of tags should be preserved
+        tags_to_add = ["contents"]
+        tags = edit_tags(current_tags=self.passed_tags, tags_to_add=tags_to_add)
+        self.assertIn("Content", tags)
+
+        # special characters
+        tags_to_remove = ["cold show"]
+        tags = edit_tags(current_tags=self.passed_tags, tags_to_remove=tags_to_remove)
+        self.assertNotIn("cold-show", tags)
+
+        tags_to_add = ["cold_show"]
+        tags = edit_tags(current_tags=self.passed_tags, tags_to_add=tags_to_add)
+        # capitalization of the new tag should be added
+        self.assertIn("COLD_SHOW", tags)
+        self.assertNotIn("cold-show", tags)
--- first_exercise/utils/tag_edit.py
+++ first_exercise/utils/tag_edit.py
@@ -1,37 +1,46 @@
-
-
-from typing import List, Optional
 import re
+from typing import List, Optional
+
 
 def remove_tags(current_tags_list: List[str], tags_to_remove: List[str]) -> List[str]:
     """The logic to remove tags, this takes care of lowercases and special chars."""
+    lowercase_tags = [tags.lower() for tags in tags_to_remove]
+    flushed_tags_to_remove = [re.sub("[-_ ]", "", tags) for tags in lowercase_tags]
+    complete_tags_list = []
+
+    for tag in current_tags_list:
+        flushed_tag = re.sub("[-_ ]", "", tag)
+        if flushed_tag.lower() not in flushed_tags_to_remove:
+            complete_tags_list.append(tag)
+    return complete_tags_list
+
+
+def add_tags(current_tags_list: List[str], tags_to_add: List[str]) -> List[str]:
+    """The logic to remove tags, this takes care of lowercases and special chars."""
     lowercase_tags = [tags.lower() for tags in current_tags_list]
-    flushed_tags = [re.sub("[-_ ]", "",  tags) for tags in lowercase_tags]
-    flushed_tags_to_remove = [re.sub("[-_ ]", "",  tags) for tags in tags_to_remove]
-    print(flushed_tags_to_remove)
+    flushed_tags = [re.sub("[-_ ]", "", tags) for tags in lowercase_tags]
+    for tag in tags_to_add:
+        flushed_tag_to_add = re.sub("[-_ ]", "", tag)
+        if flushed_tag_to_add.lower() not in flushed_tags:
+            current_tags_list.append(tag)
+        if flushed_tag_to_add.lower() in flushed_tags:
+            for current_tag in current_tags_list:
+                flushed_tag = re.sub("[-_ ]", "", current_tag)
+                if flushed_tag.lower() == flushed_tag_to_add.lower():
+                    current_tags_list.remove(current_tag)
+            current_tags_list.append(tag.upper())
+    return current_tags_list
 
 
-    # replace dash and underscrores with space
-    for tag in flushed_tags_to_remove:
-        if tag.lower() in flushed_tags:
-            flushed_tags.remove(tag)
-    return flushed_tags
-
-def edit_tags(current_tags: str, tags_to_add: Optional[List[str]] =None, tags_to_remove: Optional[List[str]] = None) -> str:
+def edit_tags(
+    current_tags: str, tags_to_add: Optional[List[str]] = None, tags_to_remove: Optional[List[str]] = None
+) -> str:
     """A Function to edit shopify arbitrary tags."""
-    list_tag = current_tags.split(', ')
-    str_to_add = ', '.join(tags_to_add)
+    list_tags = current_tags.split(', ')
     if tags_to_add:
-        list_tag.append(str_to_add)
+        list_tags = add_tags(list_tags, tags_to_add)
     if tags_to_remove:
-        list_tag = remove_tags(list_tag, tags_to_remove)
-    # convert to set to make the list unique
-    # this wouldn't matter since orders dont mater
-    set_tags = set(list_tag)
+        list_tags = remove_tags(list_tags, tags_to_remove)
+    set_tags = set(list_tags)
     tags = ', '.join(set_tags)
     return tags
-
-current_tags = "bags, pillow"
-tags_to_add = ["RuBber bird"]
-tags_to_remove = ["rubbe r"]
-print(edit_tags(current_tags=current_tags, tags_to_add=tags_to_add, tags_to_remove=tags_to_remove))
